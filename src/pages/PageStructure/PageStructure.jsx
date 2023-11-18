8d8a6f862c20ee9e18e7b853783500e62949da60
KevenSoares22 <keven.v.soares@gmail.com>
2023-11-17 21:31:17 -0300

    Primeiros ajustes

---
+++ src/pages/PageStructure/PageStructure.jsx
@@ -0,0 +1,13 @@
+
+import styles from './PageStructure.module.css'
+const PageStructure = () => {
+  return (
+    <div className={styles.pageStructure}>
+        <div className={styles.sideMenu}></div>
+        <div className={styles.content}></div>
+    
+    </div>
+  )
+}
+
+export default PageStructure
